c186bdcd1f06d6f27a9191960a6133bc555dcae7
Neizo <neizolol@hotmail.com>
2022-10-18 12:48:49 +0200

    ti1 jon, c un kdo de ma par

--- assignement_4/practical4_hopping.py
+++ assignement_4/practical4_hopping.py
@@ -2,7 +2,11 @@
 from env.leg_gym_env import LegGymEnv
 from leg_helpers import *
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+
+
 
 env = LegGymEnv(render=True, 
                 on_rack=False,    # set True to debug 
@@ -21,35 +25,45 @@ SINGLE_JUMP = False
 kpCartesian = np.diag([500,300])
 kdCartesian = np.diag([30,20])
 
+kpJoint = np.array([6,6])
+kdJoint = np.array([0.8,0.8])
+
 # define variables and force profile
 t = np.linspace(0,NUM_SECONDS,NUM_SECONDS*1000 + 1)
-Fx_max = 0     # max peak force in X direction
-Fz_max = 0     # max peak force in Z direction
-f = 0          # frequency
+Fx_max = 50    # max peak force in X direction
+Fz_max = 90     # max peak force in Z direction
+f = 1.5          # frequency
 
 if SINGLE_JUMP:
     # may want to choose different parameters
-    Fx_max = 0     # max peak force in X direction
-    Fz_max = 0     # max peak force in Z direction
-    f = 0
+    Fx_max = 50     # max peak force in X direction
+    Fz_max = 90     # max peak force in Z direction
+    f = 2
 
 # design Z force trajectory as a funtion of Fz_max, f, t
 #   Hint: use a sine function (but don't forget to remove positive forces)
-force_traj_z = np.zeros(len(t))
+force_traj_z = Fz_max * np.sin(t * 2 * np.pi * f)
+force_traj_z[force_traj_z > 0] = 0
+
+force_traj_x = Fx_max * np.sin(t * 2 * np.pi * f)
+force_traj_x[force_traj_x > 0] = 0
+
 
 if SINGLE_JUMP:
     # remove rest of profile (just keep the first peak)
-    force_traj_z = np.zeros(len(t))
+    period = int((1/f)*1000)
+    force_traj_z[period:] = 0
+    force_traj_x[period:] = 0
 
 # design X force trajectory as a funtion of Fx_max, f, t
-force_traj_x = np.zeros(len(t))
+
 
 # sample nominal foot position (can change or optimize)
-nominal_foot_pos = np.array([0.0,-0.2]) 
+nominal_foot_pos = np.array([0.0,-0.2])
 
 # keep track of max z height
-max_base_z = 0
-
+max_base_z =0
+qdes = env._robot_config.INIT_MOTOR_ANGLES
 # Track the profile: what kind of controller will you use? 
 for i in range(NUM_SECONDS*1000):
     # Torques
@@ -59,7 +73,16 @@ for i in range(NUM_SECONDS*1000):
     J, ee_pos_legFrame = jacobian_rel(env.robot.GetMotorAngles())
 
     # Add Cartesian PD (and/or joint PD? Think carefully about this, and try it out.)
-    tau += np.zeros(2) # [TODO]
+    foot_pos = ee_pos_legFrame
+    des_foot_pos = nominal_foot_pos
+    motor_vel = env.robot.GetMotorVelocities()
+    foot_vel = np.matmul(J, motor_vel)
+    des_foot_vel = 0
+    tau += np.matmul(np.transpose(J), np.matmul(kpCartesian, des_foot_pos - foot_pos) + np.matmul(kdCartesian, des_foot_vel - foot_vel))
+
+    qdes = ik_geometrical(xz=des_foot_pos)  # ik_geometrical
+
+    tau += kpJoint * (qdes - env.robot.GetMotorAngles()) + kdJoint * (0 - motor_vel)
 
     # Add force profile contribution
     tau += J.T @ np.array([force_traj_x[i], force_traj_z[i]])

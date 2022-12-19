2913e9f7960538adfbd14120c573aeb151850c1c
KingDemons98 <jonathan.henry@epfl.ch>
2022-12-19 15:25:59 +0100

    correction for graphs, stop when falling

--- projet_2/load_sb3.py
+++ projet_2/load_sb3.py
@@ -55,7 +55,7 @@ from utils.utils import plot_results
 from utils.file_utils import get_latest_model, load_all_results
 
 LEARNING_ALG = "SAC"
-EPISODE_LENGTH = 10
+EPISODE_LENGTH = 100
 
 #plot graphs or not
 ##########################################################################
@@ -88,7 +88,7 @@ elif motor_control_mode == "CARTESIAN_PD":
 elif motor_control_mode == "PD":
     interm_dir = "./logs/intermediate_models_pd/"
 else:
-    interim_dir = "./logs/intermediate_models/"
+    interm_dir = "./logs/intermediate_models/"
 
 
 # path to saved models, i.e. interm_dir + '121321105810'
@@ -97,15 +97,44 @@ else:
 # log_dir = interm_dir + 'cpg_rl_112922155242_vel_1.0'                   # this is the last one with 1.0
 # log_dir = interm_dir + 'cpg_rl_112822072518'
 # log_dir = interm_dir + 'cpg_rl_test_env120622075011'            #test avec obstacles en train
-# log_dir = interm_dir + 'CPG_120622231142'                          # fonctionne bien!
 # log_dir = interm_dir + 'CPG_120622193517'                           # 2eme avec nouveaux cpg?
 # log_dir = interm_dir + 'CPG_120822101153'                       #moving in y but not ok yet
 
 # log_dir = interm_dir + 'CPG_120822174454'            # new reward fct avec deplacement en y
 
+log_dir = interm_dir + 'CPG_cart_solve_y_121222100704'      #maybe the right for y
 
-log_dir = interm_dir + 'CPG_121022113942'            #test with in y, might work
+# log_dir = interm_dir + 'CPG_cart_solve_y_121122161834'
 
+# log_dir = interm_dir + 'CPG_y_displacement_121322165520'
+
+# log_dir = interm_dir + 'CPG_y_displacement_121422150803'            #test with in y, might work
+##########################################################################################################################################################################
+
+# log_dir = interm_dir + 'CPG_test_best_run_follow_new_rwd_121822234256'
+
+# log_dir = interm_dir + 'CPG_120822174454'            # new reward fct avec deplacement en y
+
+# log_dir = interm_dir + 'CPG_best_run_follow_121022163418'       #funny but bad
+
+# log_dir = interm_dir + 'CPG_test_old_y_121522221641'
+
+# log_dir = interm_dir + 'CPG_test_y_offset_in_hopf121722100211'          #try with y offset in hopf
+
+# log_dir = interm_dir + 'CPGCPG_test_y_offset_only_in_cart_121722174616'    # try with y offset only for cart (not joint)
+
+# log_dir = interm_dir + 'CPG_test_best_run_follow_new_rwd_121922100642'
+
+# log_dir = interm_dir + 'CPG_CPG_y_corrected_try_without_cart121522141406'   #new try without cartesian
+
+# log_dir = interm_dir + 'CPG_cpg_backward_121222204813'          #backward
+
+# log_dir = interm_dir + 'CPG_121022113942'            #test with in y, might work
+
+# log_dir = interm_dir + 'CPG_120622231142_best_run_yet'
+
+# log_dir = interm_dir + 'CPG_test_added_ztrain_to_actionS_121822171359'          #added z to action space
+############################################################################################################################################################33
 
 
 # # get latest model and normalization stats, and plot
@@ -134,28 +163,29 @@ obs = env.reset()
 episode_reward = 0
 
 # [TODO] initialize arrays to save data from simulation
-# leg_pos = np.zeros((3, 1))
-# des_leg_pos = np.zeros((3, 1))
-#
-# r = np.zeros((4, 1))
-# rdot = np.zeros((4, 1))
-# theta = np.zeros((4, 1))
-# thetadot = np.zeros((4, 1))
-# #
-# robot_pos = np.zeros((3, 1))
-# robot_speed = np.zeros((3, 1))
+leg_pos = np.zeros((1, 3))
+des_leg_pos = np.zeros((1, 3))
 
-leg_pos = np.zeros((3, 100 * EPISODE_LENGTH))
-des_leg_pos = np.zeros((3, 100 * EPISODE_LENGTH))
-############################# CPG vectors ##########################################################################
-if motor_control_mode == "CPG":
-    r = np.zeros((4, 100 * EPISODE_LENGTH))
-    rdot = np.zeros((4, 100 * EPISODE_LENGTH))
-    theta = np.zeros((4, 100 * EPISODE_LENGTH))
-    thetadot = np.zeros((4, 100 * EPISODE_LENGTH))
-#######################################################################################################################
-robot_pos = np.zeros((3, 100 * EPISODE_LENGTH))
-robot_speed = np.zeros((3, 100 * EPISODE_LENGTH))
+r = np.zeros((1, 4))
+rdot = np.zeros((1, 4))
+theta = np.zeros((1, 4))
+thetadot = np.zeros((1, 4))
+# #
+robot_pos = np.zeros((1, 3))
+robot_speed = np.zeros((1, 3))
+
+# leg_pos = np.zeros((3, 100 * EPISODE_LENGTH))
+# des_leg_pos = np.zeros((3, 100 * EPISODE_LENGTH))
+# ############################# CPG vectors ##########################################################################
+# if motor_control_mode == "CPG":
+#     r = np.zeros((4, 100 * EPISODE_LENGTH))
+#     rdot = np.zeros((4, 100 * EPISODE_LENGTH))
+#     theta = np.zeros((4, 100 * EPISODE_LENGTH))
+#     thetadot = np.zeros((4, 100 * EPISODE_LENGTH))
+# #######################################################################################################################
+# robot_pos = np.zeros((3, 100 * EPISODE_LENGTH))
+# robot_speed = np.zeros((3, 100 * EPISODE_LENGTH))
+
 
 for i in range(100 * EPISODE_LENGTH):
     action, _states = model.predict(obs, deterministic=False) # sample at test time? ([TODO]: test)
@@ -165,31 +195,47 @@ for i in range(100 * EPISODE_LENGTH):
         print('episode_reward', episode_reward)
         print('Final base position', info[0]['base_pos'])
         episode_reward = 0
+        final_time = info[0]['episode']['t']/100
+        print(f'Final time before faillure is {final_time} [s]')
+        break
 
     # [TODO] save data from current robot states for plots
 
-    if motor_control_mode == "CPG":
-        xs, ys, zs = env.envs[0].env._cpg.update()
-        des_leg_pos[:, i] = np.array([xs[0], ys[0], zs[0]])
-        r[:, i] = env.envs[0].env._cpg.get_r()
-        rdot[:, i] = env.envs[0].env._cpg.get_dr()
-        theta[:, i] = env.envs[0].env._cpg.get_theta()
-        thetadot[:, i] = env.envs[0].env._cpg.get_dtheta()
+    _, leg_pos_temp = env.envs[0].env.robot.ComputeJacobianAndPosition(0)
+    if i == 0:
+        robot_speed[0, :] = env.envs[0].env.robot.GetBaseLinearVelocity()
+        robot_pos[0, :] = env.envs[0].env.robot.GetBasePosition()
+        leg_pos[0, :] = leg_pos_temp
+        if motor_control_mode == "CPG":
+            xs, ys, zs = env.envs[0].env._cpg.update()
+            des_leg_pos[0, :] = np.array([[xs[0], ys[0], zs[0]]])
+            r[0, :] = env.envs[0].env._cpg.get_r()
+            rdot[0, :] = env.envs[0].env._cpg.get_dr()
+            theta[0, :] = env.envs[0].env._cpg.get_theta()
+            thetadot[0, :] = env.envs[0].env._cpg.get_dtheta()
+        else:
+            des_leg_pos[0, :] = env.envs[0].env._robot_config.NOMINAL_FOOT_POS_LEG_FRAME[0:3]
     else:
-        des_leg_pos[:, i] = env.envs[0].env._robot_config.NOMINAL_FOOT_POS_LEG_FRAME
+        robot_speed = np.append(robot_speed, np.array([env.envs[0].env.robot.GetBaseLinearVelocity()]), axis=0)
+        robot_pos = np.append(robot_pos, np.array([env.envs[0].env.robot.GetBasePosition()]), axis=0)
+        leg_pos = np.append(leg_pos, np.array([leg_pos_temp]), axis=0)
+        if motor_control_mode == "CPG":
+            xs, ys, zs = env.envs[0].env._cpg.update()
+            des_leg_pos = np.append(des_leg_pos, np.array([[xs[0], ys[0], zs[0]]]), axis=0)
+            r = np.append(r, np.array([env.envs[0].env._cpg.get_r()]), axis=0)
+            rdot = np.append(rdot, np.array([env.envs[0].env._cpg.get_dr()]), axis=0)
+            theta = np.append(theta, np.array([env.envs[0].env._cpg.get_theta()]), axis=0)
+            thetadot = np.append(thetadot, np.array([env.envs[0].env._cpg.get_dtheta()]), axis=0)
 
-    robot_pos[:, i] = env.envs[0].env.robot.GetBasePosition()
-    robot_speed[:, i] = env.envs[0].env.robot.GetBaseLinearVelocity()
-
-    _, leg_pos[:, i] = env.envs[0].env.robot.ComputeJacobianAndPosition(0)
+        else:
+            des_leg_pos = np.append(leg_pos, np.array([env.envs[0].env._robot_config.NOMINAL_FOOT_POS_LEG_FRAME[0:3]]), axis=0)
 
     # print(f'this is a test {env.envs[0].env.robot.GetBasePosition()}')
-    # if i == 300:
-    #     break
     
 # [TODO] make plots:
 
-t = np.arange(r.shape[1])
+
+t = np.arange(0, final_time, final_time/len(r))
 if plot_cpg and motor_control_mode == "CPG":
     colors = np.array(["b", "g", "r", "c"])
     fig = plt.figure()
@@ -199,7 +245,7 @@ if plot_cpg and motor_control_mode == "CPG":
     ax1 = subfigs[0, 0].subplots(4, sharex=True)
     subfigs[0, 0].suptitle("amplitude of oscillators (r)")
     for i, ax in enumerate(ax1):
-        ax.plot(t, r[i, :], label = 'leg' + str(i), color = colors[i])
+        ax.plot(t, r[:, i], label = 'leg' + str(i), color = colors[i])
         ax.grid(True)
         if i == 1:
             ax.set_ylabel(labels[1], loc="bottom")
@@ -210,7 +256,7 @@ if plot_cpg and motor_control_mode == "CPG":
     ax2 = subfigs[0, 1].subplots(4, sharex=True)
     subfigs[0, 1].suptitle("angles of oscillators (theta)")
     for i, ax in enumerate(ax2):
-        ax.plot(t, theta[i, :], label = 'leg' + str(i), color = colors[i])
+        ax.plot(t, theta[:, i], label = 'leg' + str(i), color = colors[i])
         ax.grid(True)
         if i == 1:
             ax.set_ylabel(labels[1], loc="bottom")
@@ -221,7 +267,7 @@ if plot_cpg and motor_control_mode == "CPG":
     ax3 = subfigs[1, 0].subplots(4, sharex=True)
     subfigs[1, 0].suptitle("derivative of amplitude (r dot)")
     for i, ax in enumerate(ax3):
-        ax.plot(t, rdot[i, :], label = 'leg' + str(i), color = colors[i])
+        ax.plot(t, rdot[:, i], label = 'leg' + str(i), color = colors[i])
         ax.grid(True)
         if i == 1:
             ax.set_ylabel(labels[1], loc="top")
@@ -232,7 +278,7 @@ if plot_cpg and motor_control_mode == "CPG":
     ax4 = subfigs[1, 1].subplots(4, sharex=True)
     subfigs[1, 1].suptitle("Angular velocity (theta dot)")
     for i, ax in enumerate(ax4):
-        ax.plot(t, thetadot[i, :], label = 'leg' + str(i), color = colors[i])
+        ax.plot(t, thetadot[:, i], label = 'leg' + str(i), color = colors[i])
         ax.grid(True)
         if i == 1:
             ax.set_ylabel(labels[1], loc="top")
@@ -250,8 +296,8 @@ if plot_foot_pos:
     ax1 = fig.subplots(3, 1, sharex=True)
     fig.suptitle("foot positions")
     for i, ax in enumerate(ax1):
-        ax.plot(t, des_leg_pos[i, :], label = "desired leg position for " + labels_positions[i])
-        ax.plot(t, leg_pos[i, :], label = "actual leg position for " + labels_positions[i], color = "r")
+        ax.plot(t, des_leg_pos[:, i], label = "desired leg position for " + labels_positions[i])
+        ax.plot(t, leg_pos[:, i], label = "actual leg position for " + labels_positions[i], color = "r")
         ax.grid(True)
         if i == 1:
             ax.set_ylabel(labels[1])
@@ -266,7 +312,7 @@ if plot_speed_pos:
     labels_speed = np.array(["Vx", "Vy", "Vz"])
     ax1 = subfigs[0].subplots(3, sharex=True)
     for i, ax in enumerate(ax1):
-        ax.plot(t, robot_speed[i, :], label = labels_speed[i])
+        ax.plot(t, robot_speed[:, i], label=labels_speed[i])
         ax.legend()
         if i == 1:
             ax.set_ylabel(labels[1])
@@ -274,11 +320,8 @@ if plot_speed_pos:
 
     labels = np.array(["x position", "y position"])
     ax2 = subfigs[1].subplots(1)
-    ax2.plot(robot_pos[0, :], robot_pos[1, :])
+    ax2.plot(robot_pos[:, 0], robot_pos[:, 1])
     plt.xlabel(labels[0])
     plt.ylabel(labels[1])
-
-    print(f'This is sparta: {robot_pos[0,:]} \n')
-    print(f'This is spartay: {robot_pos[1, :]}')
 
 plt.show()
